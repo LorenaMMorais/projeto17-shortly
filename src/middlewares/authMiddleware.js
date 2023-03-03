337ee7895226d8165790dbb6a1cab2ef0b79d040
Lorena Morais <lorenamorais@alunos.utfpr.edu.br>
2023-03-03 16:47:20 -0300

    feat: if user email exist return 409

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,7 +1,21 @@
 import userSchema from '../schemas/userSchema,js';
-
+import db from "../db.js";
 export async function validateUser(req, res, next){
     const {error} = userSchema.validate(req.body, {abortEarly: false});
     if(error) return res.status(422).send(error.details.map(detail => detail.message));
+    next();
+}
+
+export async function validateSignup(req, res, next){
+    const {name, email, password, confirmPassword} = req.body;
+
+    const signupValidation = userSchema.validate({name, email, password, confirmPassword});
+
+    if(signupValidation.error) return res.status(422).send(signupValidation.error.message);
+
+    const emailExist = await db.query(`SELECT * FROM users WHERE email = $1`, [email]);
+
+    if(emailExist.rows.length > 0) return res.status(409).send('Este email já está em uso!');
+
     next();
 }
