968ab6ece49e65342a0d0ada4321b722cf0d5612
Lorena Morais <lorenamorais@alunos.utfpr.edu.br>
2023-03-03 17:19:22 -0300

    fix: validations

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,14 +1,18 @@
 import userSchema from '../schemas/userSchema,js';
 import db from "../db.js";
 
+export async function validateUser(req, res, next){
+    const {error} = userSchema.validate(req.body, {abortEarly: false});
+
+    if(error) return res.status(422).send(error.details.map(detail => detail.message));
+
+    next();
+}
+
 export async function validateAuth(req, res, next){
     const {email} = req.body;
 
     try{
-        const {error} = userSchema.validate(req.body, {abortEarly: false});
-
-        if(error) return res.status(422).send(error.details.map(detail => detail.message));
-
         const emailExist = await db.query(`SELECT * FROM users WHERE email = $1`, [email]);
 
         if(emailExist.rowCount > 0) return res.status(409).send('Este email já está em uso!');
