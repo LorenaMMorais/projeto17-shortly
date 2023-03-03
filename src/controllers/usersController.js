f1eee2d47a001666dd6d1bcf83084ff486c0893f
Lorena Morais <lorenamorais@alunos.utfpr.edu.br>
2023-03-03 13:45:48 -0300

    feat: get userById

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,8 +1,29 @@
 import db from '../db.js';
 
 export async function getUser(req, res){
+    const {id} = req.params;
+
     try{
-        res.sendStatus(501);
+        const user = await db.query(`
+            SELECT us.id, us.name, SUM(ur."visitCount") as "visitCount"
+            FROM users us JOIN urls ur ON us.id = ur."userId"
+            WHERE us.id = $1 GROUP BY us.id;
+        `, [id]);
+
+        if (!user.rows[0]) return res.sendStatus(404);
+
+        const urls = await db.query(`
+            SELECT id, "shortUrl", url, "visitCount"
+            FROM urls WHERE urls."userId" = $1
+        `, [id]);
+        const {id: userId, name, visitCount} = user.rows[0];
+        const body = {
+            id: userId,
+            name, 
+            visitCount,
+            shortenedUrls: urls.rows.map(_mapUrls)
+        }
+        res.send(body).status(200);
     }catch(error){
         console.log(error);
         res.sendStatus(500);
@@ -21,5 +42,15 @@ export async function getRanking(req, res){
     }catch(error){
         console.log(error);
         res.sendStatus(500);
+    }
+}
+
+function _mapUrls(rowUrls) {
+    const {id, shortUrl, url, visitCount} = rowUrls;
+    return {
+        id,
+        shortUrl,
+        url,
+        visitCount
     }
 }
