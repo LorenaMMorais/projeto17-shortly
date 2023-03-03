283fd1f09eb4cb2bfab77662333dc385396178eb
Lorena Morais <lorenamorais@alunos.utfpr.edu.br>
2023-03-03 14:56:36 -0300

    chore: adjust code for deploy on render

--- src/index.js
+++ src/index.js
@@ -11,6 +11,5 @@ app.use(cors());
 app.use(json());
 app.use(router);
 
-app.listen(process.env.PORT, () => {
-    console.log('Servidor no ar na porta', process.env.PORT);
-})
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running in port: ${port}`));
